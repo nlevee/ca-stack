9327debd87edcc60c750613982e9a0c404b25917
Nicolas Levée <nlevee@newcom-regie.com>
2020-07-17 17:18:10 +0200

    feat: add systemd service for issuer

--- infra/live/azure/ca-issuer/scripts/startup-issuer.sh
+++ infra/live/azure/ca-issuer/scripts/startup-issuer.sh
@@ -29,6 +29,7 @@ echo "$(curl -Ss -H "Authorization: Bearer ${AccessToken}" -X GET \
 # update ca cert repos
 update-ca-certificates
 
+# fetch intermediate bundle certificate
 echo "Fetch Intermediate Certificate from Vault ..."
 echo "$(curl -Ss -H "Authorization: Bearer ${AccessToken}" -X GET \
     "https://ca-stack-vm-vault.vault.azure.net/secrets/CaIntermediate?api-version=7.0" | jq -r .value)" > ~/cfssl/intermediate_ca.pem
@@ -36,6 +37,7 @@ echo "$(curl -Ss -H "Authorization: Bearer ${AccessToken}" -X GET \
 # split pem
 awk 'BEGIN {c=0;} /BEGIN CERT/{c++} { print > "intermediate_ca." c ".pem"}' < intermediate_ca.pem
 mv intermediate_ca.0.pem intermediate_ca-key.pkcs8
+chmod 700 intermediate_ca-key.pkcs8
 mv intermediate_ca.1.pem intermediate_ca.pem
 
 # serve issuer server
@@ -55,4 +57,28 @@ cat <<EOF > server-config.json
 }
 EOF
 
-cfssl serve -config server-config.json -address 0.0.0.0 -port 8888 -ca intermediate_ca.pem -ca-key intermediate_ca-key.pkcs8 &
+# create system service to start issuer server
+mkdir -p /etc/systemd/system
+cat <<EOF > /etc/systemd/system/cfssl-server.service
+[Unit]
+Description=CFSSL PKI Certificate Issuer
+After=network.target
+
+[Service]
+User=ca
+ExecStart=/usr/local/sbin/cfssl \
+    -config server-config.json \
+    -address 0.0.0.0 -port 8888 \
+    -ca intermediate_ca.pem \
+    -ca-key intermediate_ca-key.pkcs8
+Restart=on-failure
+Type=simple
+WorkingDirectory=/root/cfssl
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# enable at boot and start cfssl server
+systemctl enable cfssl-server
+systemctl start cfssl-server
