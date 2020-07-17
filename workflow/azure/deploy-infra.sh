15a7ab4290756bb73bf6fb9eaf3a830748527657
Nicolas Levée <nlevee@newcom-regie.com>
2020-07-17 16:22:14 +0200

    add: variable module

---
+++ workflow/azure/deploy-infra.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+base_path=$(realpath $(dirname $0))
+infra_path=$(realpath ${base_path}/../../infra/live/azure)
+
+workflow=$1
+[ -z "${workflow}" ] && exit 1
+
+# load azure env
+source ${base_path}/.env
+
+# apply
+cd ${infra_path}/${workflow}
+
+[ ! -d ".terraform" ] \
+    && terraform init -upgrade
+
+terraform apply
