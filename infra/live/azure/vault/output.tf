980dc253d3eece2ed691deafea008ddaeb19f6dc
Nicolas Levée <nlevee@newcom-regie.com>
2020-07-22 12:28:36 +0200

    feat: add vault to store intermediate CA

--- infra/live/azure/vault/output.tf
+++ infra/live/azure/vault/output.tf
@@ -1,3 +1,7 @@
-output "vault_id" {
+output "vm_vault_id" {
   value = azurerm_key_vault.vm-vault.id
+}
+
+output "issuer_vault_id" {
+  value = azurerm_key_vault.issuer-vault.id
 }
--- infra/live/azure/vault/store.tf
+++ infra/live/azure/vault/store.tf
@@ -9,33 +9,38 @@ data "terraform_remote_state" "networks" {
   }
 }
 
-resource "azurerm_key_vault" "vm-vault" {
-  name                        = "${module.variables.azure_resource_group}-vm-vault"
-  location                    = module.variables.azure_location
-  resource_group_name         = module.variables.azure_resource_group
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_enabled         = true
-  purge_protection_enabled    = false
+# vault for ca-issuer
+resource "azurerm_key_vault" "issuer-vault" {
+  name                     = "${module.variables.azure_resource_group}-issuer-vault"
+  location                 = module.variables.azure_location
+  resource_group_name      = module.variables.azure_resource_group
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled      = true
+  purge_protection_enabled = false
 
   sku_name = "standard"
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
 
-    key_permissions = [
-      "get", "list",
-    ]
-
-    secret_permissions = [
-      "get", "list",
-    ]
-
-    storage_permissions = [
-      "get", "list",
+    # assign access from subnet
+    virtual_network_subnet_ids = [
+      data.terraform_remote_state.networks.outputs.to_vault_network_id
     ]
   }
+}
+
+# vault for vm usage
+resource "azurerm_key_vault" "vm-vault" {
+  name                     = "${module.variables.azure_resource_group}-vm-vault"
+  location                 = module.variables.azure_location
+  resource_group_name      = module.variables.azure_resource_group
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled      = true
+  purge_protection_enabled = false
+
+  sku_name = "standard"
 
   network_acls {
     default_action = "Deny"
