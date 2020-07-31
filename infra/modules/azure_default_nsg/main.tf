5202074eb09428474ec2c447a0896632bf57d3a0
Nicolas Levée <nlevee@newcom-regie.com>
2020-07-31 15:54:36 +0200

    feat: add rule for Azure Ip

--- infra/modules/azure_default_nsg/main.tf
+++ infra/modules/azure_default_nsg/main.tf
@@ -6,6 +6,19 @@ resource "azurerm_network_security_group" "default" {
 }
 
 # add defaults rules 
+resource "azurerm_network_security_rule" "allow-azure-outbound" {
+  name                        = "Allow-AzureOutBound"
+  priority                    = 4095
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "168.63.129.16"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.default.name
+}
 resource "azurerm_network_security_rule" "deny-all-inbound" {
   name                        = "Deny-AllInBound"
   priority                    = 4096
