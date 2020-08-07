08cd3f9737edf3dd89230d0e5aa00e531ea5d41b
Nicolas Levée <nlevee@newcom-regie.com>
2020-08-07 12:38:28 +0200

    feat: add name_suffix var

--- infra/live/azure/vault/variables.tf
+++ infra/live/azure/vault/variables.tf
@@ -1,11 +1,16 @@
 module "variables" {
   source = "../../../modules/variables"
 
-  workspace = var.workspace
+  workspace   = var.workspace
+  name_suffix = var.name_suffix
 }
 
 variable "workspace" {
   default = "azure-staging"
+}
+
+variable "name_suffix" {
+  default = ""
 }
 
 variable "ip_rules" {
