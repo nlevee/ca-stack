aac41d11d9e872e1c5177fb17fdd18619dbb901a
Nicolas Levée <nlevee@newcom-regie.com>
2020-08-10 16:35:29 +0200

    refactor: del firewall occurence

--- infra/live/azure/web-proxy/security.tf
+++ infra/live/azure/web-proxy/security.tf
@@ -23,32 +23,3 @@ resource "azurerm_network_security_rule" "allow-internet-outbound" {
   resource_group_name         = module.variables.azure_resource_group
   network_security_group_name = data.terraform_remote_state.networks.outputs.default_security_group_name
 }
-
-# configure firewall to allow internet dest
-resource "azurerm_firewall_network_rule_collection" "web_proxy" {
-  name                = "proxy-collection"
-  azure_firewall_name = data.terraform_remote_state.networks.outputs.proxy_firewall_name
-  resource_group_name = module.variables.azure_resource_group
-  priority            = 100
-  action              = "Allow"
-
-  rule {
-    name = "web"
-
-    source_addresses = [
-      azurerm_network_interface.web.private_ip_address,
-    ]
-
-    destination_ports = [
-      "80", "443"
-    ]
-
-    destination_addresses = [
-      "*",
-    ]
-
-    protocols = [
-      "TCP",
-    ]
-  }
-}
